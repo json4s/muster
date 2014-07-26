b7818612fec63a367aac7a19015ac0908167c706
Ivan Porto Carrero <ivan@flanders.co.nz>
2014-07-25 21:36:44 -0700

    Adds implicit conversion for jawn codec too

--- codecs/jawn/src/main/scala/muster/codec/jawn/api/package.scala
+++ codecs/jawn/src/main/scala/muster/codec/jawn/api/package.scala
@@ -1,8 +1,11 @@
-package muster.codec.jawn
-
-import muster.codec.json.ProducibleJsonOutput
+package muster
+package codec
+package jawn
 
 package object api {
 
-
+  implicit class ProducingObject[T](p: T)(implicit prod: Producer[T])  {
+      def asJson = JawnCodec.from(p)
+      def asPrettyJson = JawnCodec.Pretty.from(p)
+    }
 }
