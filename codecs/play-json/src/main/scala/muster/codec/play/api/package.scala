f4254a214d0631c96797719d764b45a418ad9ccf
Ivan Porto Carrero <ivan@flanders.co.nz>
2014-08-03 17:55:57 -0700

    adds docs for play-json integration

--- codecs/play-json/src/main/scala/muster/codec/play/api/package.scala
+++ codecs/play-json/src/main/scala/muster/codec/play/api/package.scala
@@ -8,11 +8,11 @@ import scala.util.Try
 
 package object api {
 
-  implicit class JValueProducingObject[T: Producer](p: T) {
-    def asJValue = PlayJsonCodec.from(p)
+  implicit class JsValueProducingObject[T: Producer](p: T) {
+    def asJsValue = PlayJsonCodec.from(p)
   }
 
-  implicit class JValueConsumingObject(jv: JsValue) {
+  implicit class JsValueConsumingObject(jv: JsValue) {
     @inline def as[T](implicit consumer: Consumer[T]) = PlayJsonCodec.as[T](jv)
     @inline def tryAs[T](implicit consumer: Consumer[T]) = Try(as[T])
     @inline def getAs[T](implicit consumer: Consumer[T]) = jv match {
